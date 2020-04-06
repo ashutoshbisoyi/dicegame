3c94735b9f1e89b4c15773a43f8ffb806efbca44
Ashutosh Bisoyi <62354832+ashutoshbisoyi@users.noreply.github.com>
2020-04-06 12:54:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function suffle(){
+	var audio = new Audio('sounds/sound.mp3');
+	audio.play();
 	var suffle1=Math.floor(Math.random()*6)+1;
 	document.getElementById("p1dice").setAttribute("src","images/dice"+suffle1+".png");
 	var suffle2=Math.floor(Math.random()*6)+1;
@@ -8,19 +10,20 @@ function suffle(){
 		// setTimeout(function(){ alert("Player1 is the winner!!!"); }, 500);
 		document.getElementById("header").innerText="Congo! Player1 Won !!";
 		document.getElementById("header").classList.add("newfont");
-		// document.getElementById("winnerimg1").style.display="block";
-		// document.getElementById("winnerimg2").style.display="none";
+		document.getElementById("winnerimg1").style.display="block";
+		document.getElementById("winnerimg2").style.display="none";
 	}
 	else if (suffle1<suffle2){
 		// setTimeout(function(){ alert("Player2 is the winner!!!"); }, 500);
 		document.getElementById("header").innerText="Congo! Player2 Won !!";
 		document.getElementById("header").classList.add("newfont");
-		// document.getElementById("winnerimg2").style.display="block";
+		document.getElementById("winnerimg2").style.display="block";
+		document.getElementById("winnerimg1").style.display="none";
 	}
 	else{
 		document.getElementById("header").innerText="Opps! It's a Tie !!";
 		document.getElementById("header").classList.add("newfont");
-		// document.getElementById("winnerimg2").style.display="none";
-		// document.getElementById("winnerimg1").style.display="none";
+		document.getElementById("winnerimg2").style.display="none";
+		document.getElementById("winnerimg1").style.display="none";
 	}
 }
